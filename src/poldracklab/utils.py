117be14fb6b5f97ec7ebfc47ec89dc503fb8db1c
Russell Poldrack <poldrack@gmail.com>
2024-12-30 07:38:17 -0800

    initial add

--- src/poldracklab/utils.py
+++ src/poldracklab/utils.py
@@ -1,5 +1,9 @@
 import subprocess
 from typing import Optional, List
+import requests
+import os
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
 
 
 def run_shell_cmd(
@@ -30,3 +34,23 @@ def run_shell_cmd(
     if returncode != 0:
         print(f"Command '{cmd}' failed with return code {returncode}")
     return stdout_holder
+
+
+# from http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+def DownloadFile(url, local_filename):
+    if not os.path.exists(os.path.dirname(local_filename)):
+        os.makedirs(os.path.dirname(local_filename))
+    s = requests.Session()
+    s.mount("http://", HTTPAdapter(max_retries=Retry(total=5, status_forcelist=[500])))
+
+    connect_timeout = 10.0
+
+    r = s.get(url=url, timeout=(connect_timeout, 10.0))
+    # except requests.exceptions.ConnectTimeout:
+    #    print "Too slow Mojo!"
+
+    with open(local_filename, "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+    return
